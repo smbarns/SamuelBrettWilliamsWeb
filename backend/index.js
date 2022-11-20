6d129a1bfecd3ca5b9345321a5aa8e674e549dc8
Spencer Barns <smbarns113@gmail.com>
2022-11-20 15:41:58 -0800

    API's for Bio, Films and Plays pages

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,102 @@ app.post("/api/homepage", async (req, res) => {
     }
 });
 
+app.get('/api/bio', async (req, res) => {
+    try {
+        const bio = await db.Bio.findAll({
+            attributes: ['id', 'Bio']
+        });
+        res.send(bio);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/api/bio/client_photo', async (req, res) => {
+    try {
+        const bio = await db.Bio.findAll({
+            attributes: ['id', 'client_photo']
+        });
+        res.send(bio);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.post("/api/bio"), async (req, res) => {
+    const data = req.body;
+    try {
+        const bio = await db.Bio.create(data);
+        res.send(bio);
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const films = await db.Bio.findAll({
+            include: [db.Films]
+        });
+        res.send(films);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const films = await db.Bio.findAll({
+          attributes: ['id', 'title', 'film_photo', 'director', 'writer', 'stars', 'status', 'description', 'type_film' ]
+        });
+        res.send(films);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.post("/api/films"), async (req, res) => {
+    const data = req.body;
+    try {
+        const films = await db.Films.create(data);
+        res.send(films);
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+app.get('/api/plays', async (req, res) => {
+    try {
+        const films = await db.plays.findAll({
+            include: [db.Plays]
+        });
+        res.send(plays);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.get('/api/plays', async (req, res) => {
+    try {
+        const films = await db.Bio.findAll({
+          attributes: ['id', 'title', 'play_photo', 'writer', 'description', 'play_linkl', 'link_photo', 'type_play']
+        });
+        res.send(films);
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+app.post("/api/plays"), async (req, res) => {
+    const data = req.body;
+    try {
+        const plays = await db.Plays.create(data);
+        res.send(plays);
+    } catch (err) {
+        res.send(err);
+    }
+}
+
 db.sequelize.sync().then(
     (result) => {
     app.use(express.static(__dirname + '/assets'));
