3f33b8fc6978113308a4401315438e3a43ccd6e4
mqrt1n <martin@gandr.pw>
2023-02-15 12:35:49 -0800

    #78

--- frontend/src/pages/Press.js
+++ frontend/src/pages/Press.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../styles/Press.css'
+import banner_img from '../assets/press-bg.jpg'
 
 function Press() {
   return (
-    <div className="page">Press</div>
+
+    <div className = "press">
+      <div class="test"><img className = 'bannerImg' src = {banner_img} /></div>
+        <div className = "pressBanner">
+        PRESS
+        </div>
+      </div>
   )
 }
 
