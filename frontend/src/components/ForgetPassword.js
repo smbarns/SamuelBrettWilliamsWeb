fb52181547ef181e4abdb849cb136e1b4c4a0286
Nataliya <nandriyenko@csus.edu>
2023-03-01 23:25:37 -0800

    SBW-87 #6 implement forgot password feature

---
+++ frontend/src/components/ForgetPassword.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/forgot-password", { email });
+      setMessage("Password reset email sent");
+    } catch (err) {
+      setMessage(console.log(err));
+    }
+  };
+
+  return (
+    <div>
+      <h2>Forgot Password</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
