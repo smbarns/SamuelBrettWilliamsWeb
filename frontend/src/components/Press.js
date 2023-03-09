84a2156d47097f1e9e0aa43706092d9ad7068f3f
alinajelez <alinajeleznaia@csus.edu>
2023-03-08 21:42:37 -0800

    Fixed fetch and styling for press page

--- frontend/src/components/Press.js
+++ frontend/src/components/Press.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import placeholder from '../assets/placeholder.png'
 import '../styles/PressDetails.css'
-import {Link} from 'react-router-dom'
 function Press(props) {
 
   return (
     
-    <div className = "press">
-        <Link to={`/pressdetails/${props.title}`} state={{
-            title:props.title,
-            photos:props.photos,
-            author:props.author,
-            quote:props.quote,
-            link:props.Link
-            }} ><img className = "press_image" src = {props.photos} alt=""  /></Link>
-        <span classname = "press_title"> {props.title} </span>
-        <span classname = "quote"> {props.quote} </span>
+    <div className = "pressIcon">
+        {props.press_image ? <img className = "press_image" src = {props.press_image} alt=""/> : <span className="press_title"> {props.press_title} </span>}
+        <span className = "project_title"> {props.project_name.toUpperCase()} </span>
+        <span className = "quote"> "{props.quote}" </span>
         <span className = "author"> {props.author} </span>
-        <span className = "press_link">  {props.Link} </span>
   </div>
 
   )
--- frontend/src/pages/PressPages.js
+++ frontend/src/pages/PressPages.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Press.css'
-import '../components/ButtonGroup.js'
 import PressComp from '../components/Press.js'
-//import pressData from '../samples/samplePress'
 import banner_img from '../assets/press-bg.jpg'
-import axios from 'axios';
 
 export default function PressPages() {
     
   const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -20,6 +16,26 @@ export default function PressPages() {
   })
 
   useEffect(() => {
+    fetch('http://localhost:3000/api/press')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response;
+      })
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+  }, [])
+
+  /*useEffect(() => {
     async function fetchData() {
       try {
         const response = await axios.get('/api/press');
@@ -33,7 +49,7 @@ export default function PressPages() {
       }
     }
     fetchData();
-  }, []);
+  }, []);*/
 
   
   if (loading) return <div className="page">Loading...</div>;
@@ -47,21 +63,9 @@ export default function PressPages() {
         </div>
       </div>
     <div className='page'>
-      {/* <SearchBar setContent={setPress} showAll={showAll} setActiveProp={setActiveProp} name={"press"} /> */}
-
-      {filteredData && filteredData.map((data) => (
-        <div className="pressIcons" key={data.id}>
-          
-        </div>
-      ))}
-      <div className="press">
+      <div className='press'>
         {press}
       </div>
-    </div><div className="press">
-        <div class="test"><img className='bannerImg' src={banner_img} alt=""/></div>
-        <div className="pressBanner">
-          PRESS
-        </div>
-      </div></>
+    </div></>
   )
 }
