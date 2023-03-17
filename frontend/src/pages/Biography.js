87f8094c341f0a87e503efad38c787eec867099a
mqrt1n <martin@gandr.pw>
2023-03-16 23:54:30 -0700

    SBW-101, SBW-102 #4h #Added edit for photo & des

--- frontend/src/pages/Biography.js
+++ frontend/src/pages/Biography.js
@@ -11,6 +11,10 @@ export default function Biography() {
   const [des, setDes] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [editDes, setEditDes] = useState(false);
+  const [editPic, setEditPic] = useState(false);
+  const [newDes, setNewDes] = useState('');
+  const [newPic, setNewPic] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3000/api/biopage')
@@ -28,24 +32,79 @@ export default function Biography() {
       })
   }, [])
 
+  const handleEditDes = () => {
+    setEditDes(true);
+    setNewDes(des);
+  };
+
+  const handleEditPic = () => {
+    setEditPic(true);
+    setNewPic(pic);
+  };
+
+  const handleSave = () => {
+    fetch('http://localhost:3000/api/biopage', {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ bio_des: newDes, client_photo: newPic }),
+    })
+      .then(() => {
+        setDes(newDes);
+        setPic(newPic);
+        setEditDes(false);
+        setEditPic(false);
+      })
+      .catch((error) => {
+        console.error('Error updating bio_des/client_photo: ', error);
+      });
+  };
+
   if (loading) return <div className="page">Loading...</div>;
   if (error) return <div className="page">Error!</div>;
 
   return (
-
-    <><div>
-        <img className='bannerImg' src={banner_img} />
-        <div className="banner">
-          BIOGRAPHY
-        </div>
+    <>
+      <div>
+        <img className="bannerImg" src={banner_img} />
+        <div className="banner">BIOGRAPHY</div>
       </div>
-      <div className='page'>
-        <div className="bioBody">
-          <img className='bioImg' src={pic} />
-          <span>
-            {des}
-          </span>
-        </div>
-      </div></>
-  )
+      <div className="page">
+        {editPic || editDes ? (
+          <div className="bioBody">
+            {editPic && (
+              <div>
+                <label htmlFor="pic">Client Photo URL:</label>
+                <input
+                  type="text"
+                  id="pic"
+                  value={newPic}
+                  onChange={(e) => setNewPic(e.target.value)}
+                />
+              </div>
+            )}
+            {editDes && (
+              <div>
+                <label htmlFor="des">Bio Description:</label>
+                <textarea
+                  id="des"
+                  value={newDes}
+                  onChange={(e) => setNewDes(e.target.value)}
+                ></textarea>
+              </div>
+            )}
+            <button onClick={handleSave}>Save</button>
+          </div>
+        ) : (
+          <div className="bioBody">
+            <img className="bioImg" src={pic} />
+            <span>{des}</span>
+            <div>
+              <button onClick={handleEditPic}>Edit Client Photo</button>
+              <button onClick={handleEditDes}>Edit Bio Description</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
