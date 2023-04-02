76ceb2ac16f9e594d5d47920fb304c74ee8e71ed
Nataliya <nandriyenko@csus.edu>
2023-04-02 16:10:36 -0700

    SBW-87 #fixed forgot password link route #2h and SBW-89 #created page to enter new password #6h

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+	const transporter = nodemailer.createTransport({
+		host: process.env.SMTP_HOST,
+		port: process.env.SMTP_PORT,
+		secure: false,
+		auth: {
+			user: process.env.SMTP_USER,
+			pass: process.env.SMTP_PASSWORD,
+		},
+	});
+
+	const message = {
+		from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+		to: options.email,
+		subject: options.subject,
+		html: options.message,
+	};
+
+	console.log(message);
+
+	const info = await transporter.sendMail(message);
+	console.log(info);
+
+	return info;
+};
+
+module.exports = sendEmail;
--- frontend/src/components/ForgetPassword.js
+++ frontend/src/components/ForgetPassword.js
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import '../styles/ForgotPassword.css';
+
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("/api/forgot-password", { email });
-      setMessage("Password reset email sent");
-    } catch (err) {
-      setMessage(console.log(err));
-    }
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			setLoading(true);
+			await axios.post('http://localhost:3000/api/forgot-password', { email });
+			setMessage('Password reset email sent');
+			setLoading(false);
+		} catch (err) {
+			setMessage(console.log(err.message));
+		}
+	};
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
-  );
+		<div className='forgot-password'>
+			<h2>Forgot Password</h2>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor='email'>Email:</label>
+				<input
+					type='email'
+					id='email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					placeholder='Please enter your email here'
+				/>
+				<button type='submit'>{loading ? 'Sending...' : 'Submit'}</button>
+			</form>
+			{message && <p>{message}</p>}
+		</div>
+	);
 }
