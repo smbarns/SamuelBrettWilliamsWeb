3d88bf89ffcd14fe68e31bff60d8ec86506cfcf9
RaphaelG76 <ravisityy@gmail.com>
2023-04-09 13:59:31 -0700

    Revert "Merge branch 'main' of https://github.com/smbarns/SamuelBrettWilliamsWeb"
    This reverts commit 0aa42899970d7eae08e41fbde1b5a2e39436db8e, reversing
    changes made to f4590a51753e792b8830f19e0a8015e931ae001f.

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
-import "../styles/Admin.css";
-import { NavLink } from "react-router-dom";
+
+import React, {useState} from "react"
+import '../styles/Admin.css'
+import {NavLink} from 'react-router-dom'
 
 export default function Admin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -17,67 +18,49 @@ export default function Admin() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const emailAddress = email.match(
-      /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/
-    );
+    const emailAddress = email.match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/);
 
     fetch("http://localhost:3000/login", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         email: emailAddress[0],
-        password: password,
-      }),
-    })
-      .then(function (response) {
-        if (response.status === 401) {
-          window.location.replace("http://localhost:3001/#/admin_login");
-          response.json().then((data) => {
-            setErrorMessage(data.message);
-          });
-        } else if (response.ok) {
-          localStorage.setItem("token", "1");
-
-          window.location.replace("http://localhost:3001/#/");
-        } else {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        password: password
       })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
+    })
+    .then(function(response) {
+      if (response.status === 401) {
+        window.location.replace("http://localhost:3000/#/admin_login");
+        response.json().then(data => {
+          setErrorMessage(data.message);
+        });
+      } else if (response.ok) {
+        window.location.replace("http://localhost:3000/#/");
+      } else {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+  }
 
   return (
-    <section className="admin">
-      <div className="admin__content">
-        <h3 className="admin__title">Admin Login</h3>
-        <form className="admin__form" onSubmit={handleSubmit}>
-          {errorMessage && <div>{errorMessage}</div>}
-          <input
-            className="admin__input admin__input--email"
-            placeholder="Username or Email Address"
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-          <input
-            className="admin__input admin__input--password"
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-          <div className="admin__form-actions">
-            <button className="btn btn-submit" type="submit">
-              Login
-            </button>
-            <NavLink to="/forgot_password">Forgot password</NavLink>
+      <section className='admin'>
+          <div className="admin__content">
+              <h3 className="admin__title">Admin Login</h3>
+              <form className="admin__form" onSubmit={handleSubmit}>
+                  {errorMessage && <div>{errorMessage}</div>}
+                  <input className="admin__input admin__input--email" placeholder="Username or Email Address" type="email" value={email} onChange={handleEmailChange}/>
+                  <input className="admin__input admin__input--password" placeholder="Password" type="password" value={password} onChange={handlePasswordChange} />
+                  <div className="admin__form-actions">
+                    <button className="btn btn-submit" type="submit">Login</button>
+                    <NavLink to='/forgot_password'>Forgot password</NavLink>
+                  </div>
+              </form>
           </div>
-        </form>
-      </div>
-    </section>
-  );
+      </section>
+  )
 }
--- frontend/src/pages/PressPages.js
+++ frontend/src/pages/PressPages.js
@@ -1,49 +1,55 @@
-import React, { useState, useEffect } from "react";
-import "../styles/Press.css";
-import PressComp from "../components/Press.js";
-import banner_img from "../assets/press-bg.jpg";
+import React, { useState, useEffect } from 'react';
+import '../styles/Press.css'
+import PressComp from '../components/Press.js'
+import banner_img from '../assets/press-bg.jpg'
 
 export default function PressPages() {
+    
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const press = data.map((item) => {
-    return <PressComp key={item.id} {...item} />;
-  });
+  const press = data.map(item => {
+    return(
+      <PressComp key={item.id} {...item} />
+    )
+  })
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/press")
-      .then((response) => {
+    fetch('http://localhost:3000/api/press')
+      .then(response => {
         if (response.ok) {
-          return response.json();
+          return response.json()
         }
         throw response;
       })
-      .then((data) => {
+      .then(data => {
         setData(data);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error("Error fetching data: ", error);
         setError(error);
       })
       .finally(() => {
         setLoading(false);
-      });
-  }, []);
+      })
+  }, [])
 
+  
   if (loading) return <div className="page">Loading...</div>;
   if (error) return <div className="page">Error!</div>;
 
   return (
-    <>
-      <div>
-        <img className="bannerImg" src={banner_img} />
-        <div className="banner">PRESS</div>
+    <><div>
+        <img className='bannerImg' src={banner_img} />
+        <div className="banner">
+          PRESS
+        </div>
       </div>
-      <div className="page">
-        <div className="press">{press}</div>
+    <div className='page'>
+      <div className='press'>
+        {press}
       </div>
-    </>
-  );
+    </div></>
+  )
 }
