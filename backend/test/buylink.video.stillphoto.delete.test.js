565c5b4cb5f49c49a5e95306082d7aa6c441cd87
alinajelez <alinajeleznaia@csus.edu>
2023-05-08 19:50:36 -0700

    fixed test for feature delete -buylink/video/stillphoto

--- backend/test/buylink.video.stillphoto.delete.test.js
+++ backend/test/buylink.video.stillphoto.delete.test.js
@@ -7,31 +7,78 @@ const db = require('../models');
 chai.use(chaiHttp);
 const expect = chai.expect;
 
+const data = {
+    title: "testFeatures"
+}
+
 describe("Buylink/Video/Stillphoto feature delete test", () => {
     it('it should delete a buyLink', (done) => {
         chai.request(app)
-            .get('/api/delete/buyLink')
+            .post('/api/films')
+            .send(data)
             .end(function(err, res) {
-                expect(res).to.have.status(200);
-            });
+                const buylinkData = {
+                    newBuyLink: "http://link.com",
+                    newBuyLinkImg: "http://linkImg.png",
+                    id: res.id
+                }
+                chai.request(app)
+                    .put('/api/films/add/buy_link')
+                    .send(buylinkData)
+                    .end(function(err, res1) {
+                        chai.request(app)
+                            .get(`/api/delete/buyLink?id=${res1.id}`)
+                            .end(function(err, res) {
+                                expect(res).to.have.status(200);
+                            });
+                    })
+            })
         done();
     })
 
     it('it should delete a video', (done) => {
         chai.request(app)
-            .get('/api/delete/video')
+            .post('/api/films')
+            .send(data)
             .end(function(err, res) {
-                expect(res).to.have.status(200);
-            });
+                const video = {
+                    title: res.title,
+                    videoUrl: "http://video.com"
+                }
+                chai.request(app)
+                    .post('/api/film/create/video')
+                    .send(video)
+                    .end(function(err, res1) {
+                        chai.request(app)
+                            .get(`/api/delete/video?id=${res1.id}`)
+                            .end(function(err, res) {
+                                expect(res).to.have.status(200);
+                            });
+                    })
+            })
         done();
     })
 
     it('it should delete a still photo', (done) => {
         chai.request(app)
-            .get('/api/delete/photo')
+            .post('/api/films')
+            .send(data)
             .end(function(err, res) {
-                expect(res).to.have.status(200);
-            });
+                const stillphoto = {
+                    title: res.title,
+                    photoUrl: "http://photo.png"
+                }
+                chai.request(app)
+                    .post('/api/film/create/photo')
+                    .send(stillphoto)
+                    .end(function(err, res1) {
+                        chai.request(app)
+                            .get(`/api/delete/photo?id=${res1.id}`)
+                            .end(function(err, res) {
+                                expect(res).to.have.status(200);
+                            });
+                    })
+            })
         done();
     })
 })
